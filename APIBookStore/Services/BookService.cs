281989d3749d06a0bb4053b13f741ad57ee95147
Max Pedroza <107723446+MaxPedroza@users.noreply.github.com>
2024-04-02 18:35:35 -0300

    Adicionar arquivos de projeto.

---
+++ APIBookStore/Services/BookService.cs
@@ -0,0 +1,22 @@
+﻿using APIBookStore.Models;
+using MongoDB.Driver;
+using Microsoft.Extensions.Options;
+using System.Security.AccessControl;
+
+namespace APIBookStore.Services
+{
+    public class BooksService
+    {
+        private readonly IMongoCollection <Book> _booksCollection;
+
+        public BooksService(
+            IOptions<BookStoreDatabaseSettings> bookStoreDatabaseSettings)
+        {
+            var mongoClient = new MongoClient(
+                bookStoreDatabaseSettings.Value.ConnectionString);
+            var mongoDatabase =  mongoClient().GetDatabase(
+
+                )
+        }
+    }
+}
